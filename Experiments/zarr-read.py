31e22f2335b5ae76a7463196d12bdea75d9ea1f1
Steve Pieper <pieper@isomics.com>
2024-06-26 14:37:38 -0400

    Create zarr-read.py

---
+++ Experiments/zarr-read.py
@@ -0,0 +1,22 @@
+
+notes = """
+
+object store in dashboard at https://js2.jetstream-cloud.org/project/
+
+https://js2.jetstream-cloud.org/project/containers/container/sdp-morphodepot-data
+
+https://js2.jetstream-cloud.org/project/containers/container/sdp-morphodepot-data/UF-H-158477.zarr
+
+https://js2.jetstream-cloud.org:8001/swift/v1/sdp-morphodepot-data/UF-H-158477.zarr/.zattrs
+
+
+"""
+
+
+import itk
+
+zarrURL = "https://js2.jetstream-cloud.org:8001/swift/v1/sdp-morphodepot-data/UF-H-158477.zarr"
+imageio = itk.OMEZarrNGFFImageIO.New()
+image = itk.imread(zarrURL, imageio=imageio)
+
+//itk.imwrite(image, sys.argv[2], imageio=imageio, compression=False)

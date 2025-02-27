f3c1b39e4dd9382e360994cafe04409660e304cd
Steve Pieper <pieper@isomics.com>
2025-02-27 16:37:55 -0500

    Add notes about pixi install of gh and git cross platform

---
+++ Experiments/pixi-install.py
@@ -0,0 +1,18 @@
+
+# mac / linux
+curl -fsSL https://pixi.sh/install.sh | PIXI_HOME=/tmp/pixi PIXI_NO_PATH_UPDATE=1 bash
+
+mkdir -p /tmp/pixi-test-env
+cd /tmp/pixi-test-env
+
+/tmp/pixi/bin/pixi init .
+/tmp/pixi/bin/pixi add git
+/tmp/pixi/bin/pixi add gh
+
+
+# window
+
+scriptPath = slicer.app.temporaryPath + "/pixi-install.ps1"
+slicer.util.downloadFile("https://pixi.sh/install.ps1", scriptPath)
+command = ["powershell.exe", "-ExecutionPolicy", "Bypass", "-File", scriptPath , "-PixiHome", slicer.app.temporaryPath, "-NoPathUpdate"]
+

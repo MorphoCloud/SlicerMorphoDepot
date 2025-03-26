53b160e7ca7cbde1716028abdf3e8f3d8567ae0b
Steve Pieper <pieper@isomics.com>
2025-03-26 19:16:49 -0400

    Extend experiment for gh auth login
    This works to authenticate, but when run from MorphoDepotLogic it
    does not store the credentials in a way that subsequent gh calls
    can use them.  This could be due to the git-related environment
    variables that are needed in a way that they interfere with
    finding them again.

--- Experiments/ghauth.py
+++ Experiments/ghauth.py
@@ -9,12 +9,16 @@ if os.name == "nt":
     startupInfo.wShowWindow = 0
 
 ghPath = "/Users/pieper/slicer/latest/SlicerMorphoDepot/MorphoDepot/Resources/pixi/.pixi/envs/default/bin/gh"
+ghPath = "/home/exouser/SlicerMorphoDepot/MorphoDepot/Resources/pixi/.pixi/envs/default/bin/gh"
+
+oldHOME = os.environ["HOME"]
+os.environ["HOME"] = "/home/exouser/SlicerMorphoDepot/MorphoDepot/Resources/pixi"
 
 cmd = [ghPath,
        "auth", "login",
        "--hostname", "github.com",
-       "--git-protocol", "https",
-       "--web"]
+       "--git-protocol", "https"
+]
 
 print(" ".join(cmd))
 
@@ -23,6 +27,30 @@ proc = subprocess.Popen(cmd,
                         universal_newlines=True,
                         startupinfo=startupInfo)
 
-while line = proc.stdout.readline() != '':
-    
+proc.stdout.readline() # blank
+codeLine = proc.stdout.readline()
+print(codeLine)
+code = codeLine.split()[-1]
+urlLine = proc.stdout.readline()
+print(urlLine)
+url = urlLine.split()[-1]
 
+print(code)
+
+
+webWidget = slicer.qSlicerWebWidget()
+webWidget.url = url
+webWidget.size = qt.QSize(700, 700)
+slicerPos = slicer.util.mainWindow().pos
+webWidget.pos = qt.QPoint(30 + slicerPos.x(), 50+slicerPos.y())
+webWidget.show()
+slicer.app.processEvents()
+
+
+label = qt.QLabel(f"\n\n-->\t\tLog into GitHub and then type in this code when prompted: {code}\t\t<--\n\n")
+label.pos = qt.QPoint(750+slicerPos.x(), 250+slicerPos.y())
+label.show()
+
+slicer.app.processEvents()
+
+os.environ["HOME"] = oldHOME

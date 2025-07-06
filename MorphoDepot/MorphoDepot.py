eca62c510bf6df3dea77fb8b8dc70b465edcca99
Steve Pieper <pieper@isomics.com>
2025-07-06 14:49:25 -0400

    Remove comment

--- MorphoDepot/MorphoDepot.py
+++ MorphoDepot/MorphoDepot.py
@@ -608,7 +608,6 @@ class MorphoDepotLogic(ScriptedLoadableModuleLogic):
             searchString = "--owner=@me"
         jsonFields = "title,number,isDraft,updatedAt,repository"
         candidatePRList = json.loads(self.gh(f"search prs --limit 1000 --state open --json {jsonFields} {searchString}"))
-        #candidatePRList = json.loads(self.gh(f"search prs --limit 1000 --json {jsonFields} {searchString}"))
         prList = [pr for pr in candidatePRList if pr['repository']['nameWithOwner'] in repoNamesWithOwner]
         return prList
 

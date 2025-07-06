9e9c9e7b047827a98cf359ad22ea1a01f82a1b71
muratmaga <muratmaga@outlook.com>
2025-07-06 11:13:50 -0700

    remove dangling line

--- MorphoDepot/MorphoDepot.py
+++ MorphoDepot/MorphoDepot.py
@@ -608,7 +608,7 @@ class MorphoDepotLogic(ScriptedLoadableModuleLogic):
             searchString = "--owner=@me"
         jsonFields = "title,number,isDraft,updatedAt,repository"
         candidatePRList = json.loads(self.gh(f"search prs --limit 1000 --state open --json {jsonFields} {searchString}"))
-        candidatePRList = json.loads(self.gh(f"search prs --limit 1000 --json {jsonFields} {searchString}"))
+        #candidatePRList = json.loads(self.gh(f"search prs --limit 1000 --json {jsonFields} {searchString}"))
         prList = [pr for pr in candidatePRList if pr['repository']['nameWithOwner'] in repoNamesWithOwner]
         return prList
 

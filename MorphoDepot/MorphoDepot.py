a8592f50936a880b64d59111b163550e96621ebe
Steve Pieper <pieper@isomics.com>
2025-09-25 11:53:42 -0400

    Remove download file workarlound
    See this discussion:
    
    https://github.com/Slicer/Slicer/issues/8541

--- MorphoDepot/MorphoDepot.py
+++ MorphoDepot/MorphoDepot.py
@@ -56,29 +56,6 @@ This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc
 and Steve Pieper, Isomics, Inc. and was partially funded by NIH grant 3P41RR013218-12S1.
 """)
 
-#
-# Utility
-#
-def downloadFileWorkaround(url, filePath):
-    """Workaround for https://github.com/Slicer/Slicer/issues/8541"""
-    import time
-    networkManager = qt.QNetworkAccessManager()
-    request = qt.QNetworkRequest()
-    request.setAttribute(qt.QNetworkRequest.FollowRedirectsAttribute, True)
-
-    request.setUrl(qt.QUrl(url))
-    reply = networkManager.get(request)
-
-    while not reply.isFinished():
-        # bad form, but needed to emulate synchronous
-        slicer.app.processEvents()
-
-    data = reply.readAll()
-    file = qt.QFile(filePath)
-    file.open(qt.QIODevice.WriteOnly)
-    file.write(data)
-    file.close()
-
 
 #
 # MorphoDepotWidget
@@ -1654,8 +1631,7 @@ class MorphoDepotLogic(ScriptedLoadableModuleLogic):
         volumeURL = open(volumePath).read().strip()
         nrrdPath = os.path.join(localDirectory, f"{remoteNameWithOwner.replace('/', '-')}-volume.nrrd")
         if not os.path.exists(nrrdPath):
-            #slicer.util.downloadFile(volumeURL, nrrdPath)
-            downloadFileWorkaround(volumeURL, nrrdPath)
+            slicer.util.downloadFile(volumeURL, nrrdPath)
         volumeNode = slicer.util.loadVolume(nrrdPath)
 
         # Load all segmentations
